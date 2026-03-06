dd0151a5fb13b1ec73c3e189d003e9c1df9cded8
cerenokyay <cerenokyay2002@gmail.com>
2026-03-07 02:56:19 +0300

    ilk deneme yapıldı ve 3 komut tanımlandı ama sadece yumruk kapama ve programı kapatma test edildi diğer fonksiyonlarda test edilip bir sonraki aşamaya geilmeli.python 3.11 kullanıldı ve sanal ortam oluştuurldu ve mediapipe ile uyumlu python süürümü kullanılarak sürüm uyuşmazlığı çözülmüş oldu.

--- gestureos_main/gestureos/tracking/hands_mediapipe.py
+++ gestureos_main/gestureos/tracking/hands_mediapipe.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
-import importlib
-
-# Load solutions module dynamically (works even if mp.solutions is not exposed)
-mp_solutions = importlib.import_module("mediapipe.solutions")
-mp_hands = mp_solutions.hands
-mp_drawing = mp_solutions.drawing_utils
+import mediapipe as mp
 
 
 class HandTracker:
     def __init__(self, max_hands: int = 1, det_conf: float = 0.6, track_conf: float = 0.6):
-        self.hands = mp_hands.Hands(
+        self.mp_hands = mp.solutions.hands
+        self.mp_draw = mp.solutions.drawing_utils
+
+        self.hands = self.mp_hands.Hands(
             max_num_hands=max_hands,
             model_complexity=1,
             min_detection_confidence=det_conf,
@@ -26,7 +24,7 @@ class HandTracker:
             return None, annotated
 
         hand = res.multi_hand_landmarks[0]
-        mp_drawing.draw_landmarks(annotated, hand, mp_hands.HAND_CONNECTIONS)
+        self.mp_draw.draw_landmarks(annotated, hand, self.mp_hands.HAND_CONNECTIONS)
         lm = np.array([[p.x, p.y] for p in hand.landmark], dtype=np.float32)
         return lm, annotated
 
